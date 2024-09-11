e49777ef444e650e5ec99318e71abf853c4cab0f
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-09-11 19:08:26 -0300

    leitura dos arquivos

--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
-﻿// teste
+﻿using System;
+using System.IO;
 
-Console.WriteLine("Hello, World!");
+// paths dos arquivos
+string caminhoesPath = "data/caminhoes.txt";
+string produtosPath = "data/produtos.txt";
+string unidadesPath = "data/unidades.txt";
+
+// Lê os dados dos caminhões
+using (StreamReader reader = new StreamReader(caminhoesPath))
+{
+    string line;
+    while ((line = reader.ReadLine()) != null)
+    {
+        // Process each line
+        Console.WriteLine(line);  // Example: print the line
+    }
+}
