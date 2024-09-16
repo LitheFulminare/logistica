741e097ee01e324d5753967f68b91bb5bea13785
LitheFulminare <vitor.rossafa@pucpr.edu.br>
2024-09-16 11:27:43 -0300

    misc

--- Truck.cs
+++ Truck.cs
@@ -17,7 +17,8 @@ namespace Logistica
         private List<Product> loadedProducts = new List<Product>(); // produtos que o caminhão está carregando no momento
 
         private int _remainingCapacity = 0;
-        private int _unusedCapacity = 0;
+        private int _totalUnusedCapacity = 0;
+        private int _temporaryUnusedCapacity = 0;
         private int _totalValue = 0; // reseta pra 0 depois de descarregar
 
         public Truck(string plate, int capacity)
@@ -37,6 +38,10 @@ namespace Logistica
         {
             _totalValue = 0;
         }
+        public void ResedUnused()
+        {
+            _totalUnusedCapacity = 0;
+        }
 
         // chamado por Load()
         public bool Load(Product product, int productWeight)
@@ -52,15 +57,16 @@ namespace Logistica
             }
             else // retorna false para o Program saber que não tem mais espaço
             {
-                _unusedCapacity += _remainingCapacity; // vai guardando quantos kgs ficaram livres depois de cada viagem
+                _totalUnusedCapacity += _remainingCapacity; // vai guardando quantos kgs ficaram livres depois de cada viagem
                 return false;               
             }
+            
         }
 
         // propriedades
         public string Plate => _plate;
         public int Capacity => _capacity;
-        public int UnusedCapacity => _unusedCapacity;
+        public int UnusedCapacity => _totalUnusedCapacity;
         public int UsedCapacity => _capacity - _remainingCapacity;
         public int TotalValue => _totalValue;
 
